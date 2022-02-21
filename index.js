30fb1a5a5a1c08e57b89be145d118b3d60a7a0d1
DevOps-T04 <91058125+DevOps-T04@users.noreply.github.com>
2022-02-21 09:26:50 -0500

    initial Template

---
+++ index.js
@@ -0,0 +1,31 @@
+var users = [
+    {
+        'username': 'Mohsen', 
+        'facebookLink': 'https://facebook.com',
+        'instagramLink': 'https://instagram.com',
+        'githubLink': 'https://github.com',
+        'role': 'Moderator',
+        'bio': '    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo labore voluptate saepe, repellendus nam deserunt excepturi quisquam porro! Sequi, repudiandae!'
+    },
+    {
+        'username': 'Ali', 
+        'facebookLink': 'https://facebook.com',
+        'instagramLink': 'https://instagram.com',
+        'githubLink': 'https://github.com',
+        'role': 'Admin',
+        'bio': '    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo labore voluptate saepe, repellendus nam deserunt excepturi quisquam porro! Sequi, repudiandae!'
+    },
+    {
+        'username': 'Hela', 
+        'facebookLink': 'https://facebook.com',
+        'instagramLink': 'https://instagram.com',
+        'githubLink': 'https://github.com',
+        'role': 'Guest',
+        'bio': '    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo labore voluptate saepe, repellendus nam deserunt excepturi quisquam porro! Sequi, repudiandae!'
+    }
+]
+
+// TThis function should append user's details into an html tamplate
+function createCard(user){}
+
+
